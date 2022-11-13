173d75707ae626284423c91f6089c607f9e4edda
collinator-coder <crphillips@gmail.com>
2022-11-13 14:29:25 -0500

    yez

--- J.A.R.V.I.S.py
+++ J.A.R.V.I.S.py
@@ -1,55 +1,88 @@
-from vosk import Model, KaldiRecognizer
-import wave
+
+#!/usr/bin/env python3
+
+import argparse
+import queue
+import sys
+import sounddevice as sd
 import json
-import os
-from posixpath import dirname
-dirname = os.path.dirname(__file__)
-in_file_name = os.path.join(dirname, 'Wav', 'Recording_mono.wav')
-out_file_text = os.path.join(dirname, 'Text')
-out_file_results = os.path.join(dirname, 'Results')
-'''
-this script reads a mono wav file (inFileName) and writes out a json file (outfileResults) with the speech to text conversion results.  It then writes out another json file (outfileText) that only has the "text" values.
-'''
+import requests
 
-## ffmpeg -i Recording.wav -ac 1 Recording_mono.wav
+from vosk import Model, KaldiRecognizer
 
-wf = wave.open(in_file_name, "r")
+q = queue.Queue()
 
-# initialize a str to hold results
-results = ""
-text_results = []
+def int_or_str(text):
+    """Helper function for argument parsing."""
+    try:
+        return int(text)
+    except ValueError:
+        return text
 
-# build the model and recognizer objects.
-model = Model(dirname)
-recognizer = KaldiRecognizer(model, wf.getframerate())
-recognizer.SetWords(True)
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
 
-while True:
-    data = wf.readframes(4000)
-    if len(data) == 0:
-        break
-    if recognizer.AcceptWaveform(data):
-        recognizerResult = recognizer.Result()
-        results = results + recognizerResult
-        # convert the recognizerResult string into a dictionary  
-        result_dict = json.loads(recognizerResult)
-        # save the 'text' value from the dictionary into a list
-        text_results.append(result_dict.get("text", ""))
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument(
+    "-l", "--list-devices", action="store_true",
+    help="show list of audio devices and exit")
+args, remaining = parser.parse_known_args()
+if args.list_devices:
+    print(sd.query_devices())
+    parser.exit(0)
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[parser])
+parser.add_argument(
+    "-f", "--filename", type=str, metavar="FILENAME",
+    help="audio file to store recording to")
+parser.add_argument(
+    "-d", "--device", type=int_or_str,
+    help="input device (numeric ID or substring)")
+parser.add_argument(
+    "-r", "--samplerate", type=int, help="sampling rate")
+args = parser.parse_args(remaining)
 
-##    else:
-##        print(recognizer.PartialResult())
-        
-# process "final" result
-results = results + recognizer.FinalResult()
-result_dict = json.loads(recognizer.FinalResult())
-text_results.append(result_dict.get("text", ""))
+try:
+    if args.samplerate is None:
+        device_info = sd.query_devices(args.device, "input")
+        # soundfile expects an int, sounddevice provides a float:
+        args.samplerate = int(device_info["default_samplerate"])
 
-# write results to a file
-filestr='JVStext'
-file="JVStext"
-with open(out_file_results, 'w') as output:
-    print(results, file, output)
+    model = Model(lang="en-us")
 
-# write text portion of results to a file
-with open(out_file_text, filestr) as output:
-    print(json.dumps(text_results, indent=4))
+    if args.filename:
+        dump_fn = open(args.filename, "wb")
+    else:
+        dump_fn = None
+
+    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device,
+            dtype="int16", channels=1, callback=callback):
+        print("#" * 80)
+        print("Press Ctrl+C to stop the recording")
+        print("#" * 80)
+
+        rec = KaldiRecognizer(model, args.samplerate)
+        while True:
+            data = q.get()
+            if rec.AcceptWaveform(data):
+                # print(rec.Result())
+                result_text = rec.Result()
+                # print(result_text)
+                parsed_data = json.loads(result_text)
+                # print(parsed_data['text'])
+                if parsed_data['text'].startswith('jarvis'):
+                    print(parsed_data['text'].replace('jarvis ', ''))
+                    r = requests.post('http://openhab.local:8080/rest/habot/chat', auth=('admin', 'JASBuJxnuFq4JY3'), data=(parsed_data['text']))
+            if dump_fn is not None:
+                dump_fn.write(data)
+
+except KeyboardInterrupt:
+    print("\nDone")
+    parser.exit(0)
+except Exception as e:
+    parser.exit(type(e).__name__ + ": " + str(e))
--- Wav/JVSNoise.py
+++
@@ -1,6 +0,0 @@
-import statsmodels.api as sm
-
-y_lowess = sm.nonparametric.lowess(y, x, frac = 0.3)  # 30 % lowess smoothing
-
-plt.plot(y_lowess[:, 0], y_lowess[:, 1], 'b')  # some noise removed
-plt.show()
